87b3142a253b91c54fc8e7d61b72f410b9c62e0c
keyaira91 <keyaira22@hotmail.com>
2021-11-07 18:00:50 -0500

    a required fix

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const journal = document.getElementById('journal');
 
 document.querySelector("button").addEventListener("click", function(e) {
     e.preventDefault();
+    addEntry()
     newNote.value = "";
-})
+});
 
-function addEntry(e) { 
-    let notes = document.createElement("p");
-    notes.setAttribute("class", "col-3 mr-3 text-break");
-    notes.setAttribute("id", "notes");
-
-    notes.textContent = newNote.value;
-    journal.appendChild(notes);
-
+function addEntry() { 
+    if (newNote.value.length === 0) {
+        alert("Please make an entry!")
+    } else {
+        let notes = document.createElement("p");
+        notes.setAttribute("class", "col-3 mr-3 text-break");
+        notes.setAttribute("id", "notes");
+        notes.textContent = newNote.value;
+        journal.appendChild(notes);
+    }
 }
+
